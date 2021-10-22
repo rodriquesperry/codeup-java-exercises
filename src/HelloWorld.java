d5d688109d67e904bdb8e0047b7d877db2241c2d
Rodriques <rodriques.perry@gmail.com>
2021-10-22 09:52:23 -0500

    Java Syntax Types and Variables Exercises number 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World!");
+
+        int myFavoriteNumber = 10;
+        System.out.println("\nMy favorite number is " + myFavoriteNumber);
     }
 }
