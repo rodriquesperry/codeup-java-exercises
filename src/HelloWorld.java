bbe139a0b084a8f76de8ffa98857f9265289d9d6
Rodriques <rodriques.perry@gmail.com>
2021-10-22 10:22:12 -0500

    Java Syntax Types and Variables Exercises through number 13

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,9 +17,18 @@ public class HelloWorld {
         System.out.println(++x);
         System.out.println(x);
 
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        int three = (int) o;
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
 
 
     }
