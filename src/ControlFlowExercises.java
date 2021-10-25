900c81e2f96e8318e8600cf1741d5312200760a5
Rodriques <rodriques.perry@gmail.com>
2021-10-25 11:17:44 -0500

    complete: Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i);
+//            i++;
+//        }
+        int i = 0;
+        do {
+
             System.out.println(i);
-            i++;
-        }
+            i += 2;
 
-
+        } while (i <= 100);
     }
 }
